fd6547c0420237cc641f4e4dd2b4dd34e2d01794
Mautc <mauriliotcoloda2006@gmail.com>
2025-11-27 00:02:10 -0300

    Commit 16

--- src/Customer.java
+++ src/Customer.java
@@ -17,45 +17,23 @@ public class Customer {
         return _name;
     }
 
+    // NOVO MÉTODO: Retorna a lista de aluguéis para a classe Statement acessar
+    public Enumeration getRentals() {
+        return _rentals.elements();
+    }
+
+    // MÉTODO DELEGADO: Apenas chama a nova classe TextStatement
     public String statement() {
-        Enumeration rentals = _rentals.elements();
-        String result = "Rental Record for " + getName() + "\n";
-
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
-        }
-
-        // add footer lines
-        result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                " frequent renter points";
-        return result;
+        return new TextStatement().value(this);
     }
 
-    // NOVO MÉTODO: Statement em HTML
+    // MÉTODO DELEGADO: Apenas chama a nova classe HtmlStatement
     public String htmlStatement() {
-        Enumeration rentals = _rentals.elements();
-        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += each.getMovie().getTitle()+ ": " +
-                    String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-
-        // add footer lines
-        result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" +
-                String.valueOf(getTotalFrequentRenterPoints()) +
-                "</EM> frequent renter points<P>";
-        return result;
+        return new HtmlStatement().value(this);
     }
 
-    private double getTotalCharge() {
+    // MUDANÇA: MÉTODO AGORA É PÚBLICO
+    public double getTotalCharge() {
         double result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
@@ -65,7 +43,8 @@ public class Customer {
         return result;
     }
 
-    private int getTotalFrequentRenterPoints(){
+    // MUDANÇA: MÉTODO AGORA É PÚBLICO
+    public int getTotalFrequentRenterPoints(){
         int result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
