d999b2b53edc528cb09cb37f0227d27d2bd984f1
Mautc <mauriliotcoloda2006@gmail.com>
2025-11-26 23:53:40 -0300

    Commit 8

--- src/Customer.java
+++ src/Customer.java
@@ -18,16 +18,11 @@ public class Customer {
     }
 
     public String statement() {
-        // REMOVIDA: double totalAmount = 0;
-        // REMOVIDA: int frequentRenterPoints = 0;
-
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-
-            // LÓGICA DE ACUMULAÇÃO REMOVIDA
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -35,10 +30,28 @@ public class Customer {
         }
 
         // add footer lines
-        // SUBSTITUÍDA: totalAmount e frequentRenterPoints por chamadas a métodos
         result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " frequent renter points";
+        return result;
+    }
+
+    // NOVO MÉTODO: Statement em HTML
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle()+ ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        // add footer lines
+        result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
         return result;
     }
 
