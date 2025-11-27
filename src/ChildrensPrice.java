ccd276f6061174d356dbb1e58c78106f52c27030
Mautc <mauriliotcoloda2006@gmail.com>
2025-11-26 23:59:00 -0300

    Commit 13

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -2,4 +2,12 @@ public class ChildrensPrice extends Price {
     public int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    // IMPLEMENTAÇÃO ESPECÍFICA PARA INFANTIL
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
 }
